6aa291d89b1e7e9620403c4a616b6276b1e0b7b0
jkind889 <jova.kind@bison.howard.edu>
2025-12-14 20:32:50 -1000

    Addition - getPassword function
    Added the function that will get the password from the user
    and then input that password inside a file

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+
+class passData
+{
+    public:
+
+
+
+
+    private:
+    std::string site;
+    std::string password;
+
+    void getPassword(const std::string& site, const std::string& password)
+    {
+        std::ofstream file("passwords.txt", std::ios::app);
+        if(file.is_open())
+        {
+            file << site << " " << password << '\n';
+        }
+        else {
+            std::cerr << "File wasnt opened" <<'\n';
+        }
+        file.close();
+
+    }
+
+    // void fetchPasswords()
+    // {
+    //     std::ifstream file("passwords.txt");
+    //     if(file.is_open())
+    //     {
+    //         std::string site, password;
+
+    //     }
+    //     else
+    //     {
+    //         std::cerr << "No go" <<'\n'
+    //     }
+
+    // }
+
+}
+
+
+
+int main()
+{
+
+}
+
